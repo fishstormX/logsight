8307c0d14266fd4feee1817c73048092c9a7a812
fishstormX <admin@fishmaple.cn>
2020-04-12 12:22:40 +0800

    补充setting模块 h2驱动版本

--- logsight-core/src/main/java/cn/fishmaple/logsight/util/TimeUtil.java
+++ logsight-core/src/main/java/cn/fishmaple/logsight/util/TimeUtil.java
@@ -1,4 +1,23 @@
 package cn.fishmaple.logsight.util;
 
+import org.springframework.util.Assert;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class TimeUtil {
+    private static final String DEFAULT_PATTERN = "yyyy-MM-dd HH:mm:ss";
+    private static ThreadLocal<SimpleDateFormat> simpleDateFormatThreadLocal = new ThreadLocal();
+
+    public static void initedFormatter(String pattern){
+        simpleDateFormatThreadLocal.set(new SimpleDateFormat(pattern));
+    }
+
+    public static void initedFormatter(){
+        simpleDateFormatThreadLocal.set(new SimpleDateFormat(DEFAULT_PATTERN));
+    }
+
+    public static String formatTimeUnchecked(Date date){
+        Assert.notNull(simpleDateFormatThreadLocal.get(),"simpleDateFormatThreadLocal not Init Error");
+        return simpleDateFormatThreadLocal.get().format(date);
+    }
 }
