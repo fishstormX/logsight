c86f3ead728059177f2e74f24cc7a19f5e7a1676
fishstormX <admin@fishmaple.cn>
2020-04-25 22:35:21 +0800

    更新sql

--- logsight-dao/src/main/resources/db/logsight.sql
+++ logsight-dao/src/main/resources/db/logsight.sql
@@ -1,5 +1,3 @@
-DROP TABLE IF EXISTS `log_field`;
-DROP TABLE IF EXISTS `log_field_file`;
 CREATE TABLE IF NOT EXISTS `log_field`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `path` varchar(255) DEFAULT NULL COMMENT 'Log files path',
@@ -15,11 +13,19 @@ CREATE TABLE IF NOT EXISTS `log_field_file`  (
   `path_name` varchar(255) DEFAULT NULL,
   `file_size` bigint DEFAULT 0,
   `timeline` timestamp COMMENT 'Last update time',
-  `status` int default 1 ,
+  `status` int DEFAULT 0,
+  `last_scan` timestamp ,
+  `prev_size` bigint DEFAULT 0,
   PRIMARY KEY (`id`)
 );
+CREATE TABLE IF NOT EXISTS `report_file`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `path_name` varchar(255) DEFAULT NULL,
+  `file_size` bigint DEFAULT 0,
+  `timespan` timestamp COMMENT 'Last update time',
+  PRIMARY KEY (`id`),
+  INDEX path_name_idx ( `path_name` ),
+  INDEX field_id_idx ( `field_id` )
+);
 ALTER TABLE `log_field` ADD UNIQUE (`path`);
-INSERT INTO `log_field`(`path`,`create_time`,`file_count`, `status`) VALUES('/data/*',CURRENT_TIME  ,12,0);
-
-
-
+ALTER TABLE `log_field_file` ADD UNIQUE (`field_id`,`path_name`);
