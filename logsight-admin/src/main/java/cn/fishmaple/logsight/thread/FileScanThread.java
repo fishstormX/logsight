06d991f469befc1457e9cdaad1422dcf7df67c84
fishstormX <admin@fishmaple.cn>
2020-04-25 22:37:22 +0800

    文件扫描逻辑增强

--- logsight-admin/src/main/java/cn/fishmaple/logsight/thread/FileScanThread.java
+++ logsight-admin/src/main/java/cn/fishmaple/logsight/thread/FileScanThread.java
@@ -1,29 +1,36 @@
 package cn.fishmaple.logsight.thread;
 
+import cn.fishmaple.logsight.dao.dto.FileReportDTO;
 import cn.fishmaple.logsight.dao.dto.LogFieldFileDTO;
+import cn.fishmaple.logsight.dao.mapper.FileReportMapper;
 import cn.fishmaple.logsight.dao.mapper.LogFieldFileMapper;
 import cn.fishmaple.logsight.dao.mapper.LogFieldMapper;
 import cn.fishmaple.logsight.dao.object.IdPagedStat;
+import cn.fishmaple.logsight.dao.object.PagedStat;
 import cn.fishmaple.logsight.handler.FileScanHandler;
 import cn.fishmaple.logsight.service.ApplicationContextProvider;
 import cn.fishmaple.logsight.util.FileUtil;
+import cn.fishmaple.logsight.util.TimeUtil;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.Date;
 import java.util.List;
 
 @Service
 @DependsOn(value = {"logFieldMapper"})
 public class FileScanThread extends Thread{
     LogFieldFileMapper logFieldFileMapper;
+    FileReportMapper fileReportMapper;
     @PostConstruct
     public void init(){
        new FileScanThread().start();
     }
     private void initComponents(){
-        while (null==logFieldFileMapper){
+        while (null==logFieldFileMapper||null==fileReportMapper){
             logFieldFileMapper = ApplicationContextProvider.getBean(LogFieldFileMapper.class);
+            fileReportMapper = ApplicationContextProvider.getBean(FileReportMapper.class);
             try {
                 Thread.sleep(10000);
             } catch (InterruptedException e) {
@@ -34,20 +41,31 @@ public class FileScanThread extends Thread{
     public void run(){
         initComponents();
         while (true) {
-            Long startId = 0L;
-            IdPagedStat idPagedStat = new IdPagedStat();
-            idPagedStat.setStartId(startId);
-            idPagedStat.setEndId(startId+100);
-            List<LogFieldFileDTO> logFieldFileDTOList = logFieldFileMapper.getDistinctFilesByPaged(idPagedStat);
+            Long start = 0L;
+            PagedStat pagedStat = new PagedStat();
+            pagedStat.setStart(start);
+            pagedStat.setCount(100L);
+            List<LogFieldFileDTO> logFieldFileDTOList = logFieldFileMapper.getDistinctFilesByPaged(pagedStat);
             while(!logFieldFileDTOList.isEmpty()) {
-                idPagedStat.setStartId(startId);
-                idPagedStat.setEndId(startId+100);
                 for(LogFieldFileDTO logFieldFileDTO:logFieldFileDTOList){
                     logFieldFileDTO.setFileSize(FileUtil.getFileLength(logFieldFileDTO.getPathName()));
-                    logFieldFileMapper.updateFileSize(logFieldFileDTO);
+                    Date earlyHour = TimeUtil.getEarlyHour();
+                    if(!logFieldFileDTO.getLastScan().equals(earlyHour)){
+                        logFieldFileDTO.setLastScan(earlyHour);
+                        logFieldFileDTO.setPrevSize(logFieldFileDTO.getFileSize());
+                        logFieldFileMapper.updateFileSizeWithPrev(logFieldFileDTO);
+                        FileReportDTO fileReportDTO = new FileReportDTO();
+                        fileReportDTO.setPathName(logFieldFileDTO.getPathName());
+                        fileReportDTO.setTimespan(earlyHour);
+                        fileReportDTO.setFileSize(logFieldFileDTO.getFileSize());
+                        fileReportMapper.addReport(fileReportDTO);
+                    }else {
+                        logFieldFileMapper.updateFileSize(logFieldFileDTO);
+                    }
                 }
-                startId+=100;
-                logFieldFileDTOList = logFieldFileMapper.getDistinctFilesByPaged(idPagedStat);
+                start+=100;
+                pagedStat.setStart(start);
+                logFieldFileDTOList = logFieldFileMapper.getDistinctFilesByPaged(pagedStat);
             }
             try {
                 Thread.sleep(10000);
