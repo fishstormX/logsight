c978c582dafe22a201792d63df479e73063d62cd
fishstormX <stormnetwork@163.com>
2020-06-27 13:39:50 +0800

    适配部分特殊字符在grep命令下的异常

--- logsight-admin/src/main/java/cn/fishmaple/logsight/analyser/commandAnalyser/DefaultCommandAnalyser.java
+++ logsight-admin/src/main/java/cn/fishmaple/logsight/analyser/commandAnalyser/DefaultCommandAnalyser.java
@@ -1,11 +1,35 @@
 package cn.fishmaple.logsight.analyser.commandAnalyser;
 
+import cn.fishmaple.logsight.analyser.object.SearchAction;
+import cn.fishmaple.logsight.object.SearchResult;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class DefaultCommandAnalyser extends AbstractCommandAnalyser{
     @Override
     public String baseSearchTemplate() {
-        return "tac '{file}'|grep '{target}'";
+        return "tac \"{file}\"|grep \"{target}\"";
+    }
+    @Override
+    public List<SearchResult> baseSearch(SearchAction searchAction){
+        List<SearchResult> list = new ArrayList<>();
+        for(String file:searchAction.getFiles()){
+            String s = processThread.execute(baseSearchTemplate()
+                    .replace("{file}",file)
+                    .replace("{target}",targetHandler(searchAction.getTarget())));
+            list.add(new SearchResult(file, Arrays.asList(s.split("\\\n")))) ;
+        }
+        return list;
+    }
+    private String targetHandler(String target){
+        return target
+                .replace("\"","\\\"")
+                .replace("\\","\\\\\\\\")
+                .replace("[","\\[")
+                .replace("]","\\]");
     }
 }
